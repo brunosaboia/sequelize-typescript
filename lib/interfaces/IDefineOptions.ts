cadb1daae6630970ba6df0d7298e84d8edcae51e
Bruno Saboia <brunosaboia@creactive.com.br>
2017-09-21 07:41:53 -0300

    Add audit field to model options

--- lib/interfaces/IDefineOptions.ts
+++ lib/interfaces/IDefineOptions.ts
@@ -9,4 +9,5 @@ export interface IDefineOptions extends DefineOptions<any> {
    * Set to true or a string with the attribute name you want to use to enable.
    */
   version?: boolean | string;
+  audit?: boolean;
 }
